c2ab21b6a289fb6647292f07408ec1853025f4c2
Ashen Abeysekara <ashendul@gmail.com>
2024-07-02 14:13:12 +0530

    added the labels, but needs to remove those labels when clear button is pressed

--- main.py
+++ main.py
@@ -1,17 +1,76 @@
 import string
-import getpass
 from tkinter import *
 import tkinter as tk
 
 window = Tk()
 window.title("Password Strength Checker")
 window.geometry("500x400")
+window.resizable(True, True)
 
 def check_password():
-    pass
+    password = password_box.get()
+    strength = 0
+    remarks = ''
+    lower_count = upper_count = num_count = wspace_count = special_count = 0
+
+    for char in list(password):
+        if char in string.ascii_lowercase:
+            lower_count += 1
+        elif char in string.ascii_uppercase:
+            upper_count +=1
+        elif char in string.digits:
+            num_count += 1
+        elif char == ' ':
+            wspace_count +=1
+        else:
+            special_count +=1
+
+    if lower_count >= 1:
+        strength +=1
+    if upper_count >= 1:
+        strength +=1
+    if num_count >= 1:
+        strength +=1
+    if wspace_count>=1:
+        strength +=1
+    if special_count>=1:
+        strength +=1
+
+    if strength == 1:
+        remarks = "Very Bad Password!!! Change ASAP"
+    elif strength == 2:
+        remarks = "Not A Good Password!!! Change ASAP"
+    elif strength ==3:
+        remarks = "It's a weak password, consider changing"
+    elif strength == 4:
+        remarks = "It's a hard password, but can be better"
+    elif strength == 5:
+        remarks = "A very strong password"    
+
+    lower_label = Label(window, text=f"{lower_count} lowercase characters")
+    lower_label.pack()
+
+    upper_label = Label(window, text=f"{upper_count} uppercase characters")
+    upper_label.pack()
+
+    num_label = Label(window, text=f"{num_count} numeric characters")
+    num_label.pack()
+
+    wspace_label = Label(window, text=f"{wspace_count} whitespace characters")
+    wspace_label.pack()
+
+    special_label = Label(window, text=f"{special_count} special characters")
+    special_label.pack()
+
+    strength_label = Label(window, text=f"Password Strength:{strength}")
+    strength_label.pack(pady=20)
+
+    remarks_label = Label(window, text=f"Hint: {remarks}")
+    remarks_label.pack()
 
 def clear_box():
     password_box.delete(0, END)
+    window.
 
 lf = LabelFrame(window, text="Enter the password: ")
 lf.pack(pady=20)
@@ -27,9 +86,6 @@ check_button.grid(row=0, column=0, padx=10)
 
 clear_button = Button(button_frame, text="Clear Password", command=clear_box)
 clear_button.grid(row=0, column=1, padx=10)
-
-pw_box = Entry(window, text='', font=("Poppins, 12"), bd=0, bg="systembuttonface", width=60, justify=LEFT)
-pw_box.pack(pady=5)
 
 window.mainloop()
 
