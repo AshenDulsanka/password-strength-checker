575a1240c1f5b152735793d44ce25f1a113228b9
Ashen Abeysekara <ashendul@gmail.com>
2024-07-02 14:24:22 +0530

    need to fix the clear button functionality

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ window.title("Password Strength Checker")
 window.geometry("500x400")
 window.resizable(True, True)
 
+lower_label = Label(window)
+upper_label = Label(window)
+num_label = Label(window)
+special_label = Label(window)
+strength_label = Label(window) 
+remarks_label = Label(window)
+
 def check_password():
     password = password_box.get()
     strength = 0
     remarks = ''
-    lower_count = upper_count = num_count = wspace_count = special_count = 0
+    lower_count = upper_count = num_count = special_count = 0
 
     for char in list(password):
         if char in string.ascii_lowercase:
@@ -20,8 +27,6 @@ def check_password():
             upper_count +=1
         elif char in string.digits:
             num_count += 1
-        elif char == ' ':
-            wspace_count +=1
         else:
             special_count +=1
 
@@ -31,20 +36,16 @@ def check_password():
         strength +=1
     if num_count >= 1:
         strength +=1
-    if wspace_count>=1:
-        strength +=1
     if special_count>=1:
         strength +=1
 
     if strength == 1:
-        remarks = "Very Bad Password!!! Change ASAP"
-    elif strength == 2:
         remarks = "Not A Good Password!!! Change ASAP"
-    elif strength ==3:
+    elif strength ==2:
         remarks = "It's a weak password, consider changing"
-    elif strength == 4:
+    elif strength == 3:
         remarks = "It's a hard password, but can be better"
-    elif strength == 5:
+    elif strength == 4:
         remarks = "A very strong password"    
 
     lower_label = Label(window, text=f"{lower_count} lowercase characters")
@@ -56,13 +57,10 @@ def check_password():
     num_label = Label(window, text=f"{num_count} numeric characters")
     num_label.pack()
 
-    wspace_label = Label(window, text=f"{wspace_count} whitespace characters")
-    wspace_label.pack()
-
     special_label = Label(window, text=f"{special_count} special characters")
     special_label.pack()
 
-    strength_label = Label(window, text=f"Password Strength:{strength}")
+    strength_label = Label(window, text=f"Password Strength: {strength}")
     strength_label.pack(pady=20)
 
     remarks_label = Label(window, text=f"Hint: {remarks}")
@@ -70,7 +68,12 @@ def check_password():
 
 def clear_box():
     password_box.delete(0, END)
-    window.
+    lower_label.destroy()
+    upper_label.destroy()
+    num_label.destroy()
+    special_label.destroy()
+    strength_label.destroy()
+    remarks_label.destroy()
 
 lf = LabelFrame(window, text="Enter the password: ")
 lf.pack(pady=20)
@@ -88,76 +91,3 @@ clear_button = Button(button_frame, text="Clear Password", command=clear_box)
 clear_button.grid(row=0, column=1, padx=10)
 
 window.mainloop()
-
-# def check_pwd():
-#     password = getpass.getpass("Enter Password: ")
-#     strength = 0
-#     remarks = ''
-#     lower_count = upper_count = num_count = wspace_count = special_count = 0
-
-#     for char in list(password):
-#         if char in string.ascii_lowercase:
-#             lower_count += 1
-#         elif char in string.ascii_uppercase:
-#             upper_count +=1
-#         elif char in string.digits:
-#             num_count += 1
-#         elif char == ' ':
-#             wspace_count +=1
-#         else:
-#             special_count +=1
-
-#     if lower_count >= 1:
-#         strength +=1
-#     if upper_count >= 1:
-#         strength +=1
-#     if num_count >= 1:
-#         strength +=1
-#     if wspace_count>=1:
-#         strength +=1
-#     if special_count>=1:
-#         strength +=1
-
-#     if strength == 1:
-#         remarks = "Very Bad Password!!! Change ASAP"
-#     elif strength == 2:
-#         remarks = "Not A Good Password!!! Change ASAP"
-#     elif strength ==3:
-#         remarks = "It's a weak password, consider changing"
-#     elif strength == 4:
-#         remarks = "It's a hard password, but can be better"
-#     elif strength == 5:
-#         remarks = "A very strong password"
-
-#     print('Your password has: ')
-#     print(f"{lower_count} lowercase characters")
-#     print(f"{upper_count} uppercase characters")
-#     print(f"{num_count} numeric characters")
-#     print(f"{wspace_count} whitespace characters")
-#     print(f"{special_count} special characters")
-
-#     print(f"Password Strength:{strength}")
-#     print(f"Hint: {remarks}")
-
-# def ask_pwd(another_pwd=False):
-#     valid = False
-#     if another_pwd:
-#         choice=input('Do you want to enter another pwd (y/n): ')
-#     else:
-#         choice=input('Do you want to check pwd (y/n): ')
-
-#     while not valid:
-#         if choice.lower() == 'y':
-#             return True
-#         elif choice.lower() == 'n':
-#             return False
-#         else:
-#             print('Invalid, Try Again')
-#             break
-
-# if __name__ == '__main__':
-#     print('+++ welcome to PWD checker +++')
-#     ask_pw = ask_pwd()
-#     while check_pwd:
-#         check_pwd()
-#         ask_pw = ask_pwd(True)
